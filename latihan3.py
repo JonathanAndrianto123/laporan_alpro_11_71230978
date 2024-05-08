931abccd1c33ea23df736df493e1bd5753232a9a
JonathanAndrianto123 <143707322+JonathanAndrianto123@users.noreply.github.com>
2024-05-08 22:10:36 +0700

    Update latihan3.py

--- latihan3.py
+++ latihan3.py
@@ -7,11 +7,3 @@ with open ("mbox-short.txt", "r") as file :
             hasil[email] = hasil.get(email, 0) + 1
 
 print(hasil)
-# word = 'brontosaurus'
-# d = dict()
-# for c in word:
-#     if c not in d:
-#         d[c] = 1
-#     else:
-#         d[c] = d[c] + 1
-# print(d)
